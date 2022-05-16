af69d9fca0fdb4a5c52ad8131e76827ec5e4891b
MouradFrz <91010360+MouradFrz@users.noreply.github.com>
2022-05-16 22:29:23 +0100

    Update register.blade.php

--- resources/views/owners/register.blade.php
+++ resources/views/owners/register.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Signup</title>
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <style>
       .hide{
