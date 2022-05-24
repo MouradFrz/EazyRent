842d98232f7d8d2a85d2f7a7d650afd9dca571de
hacene <cenha06@gmail.com>
2022-05-24 21:16:10 +0100

    commit ta3 hkak berk

--- resources/views/users/offer.blade.php
+++ resources/views/users/offer.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.userLayout')
 @section('content')
-{{$vehicule}}
 <div class="offer">
   <div class="container">
-    <div class="offer_media"></div>
+    <div class="offer_header">
+      <h1>{{$vehicule->brand}}</h1>
+      <h1>{{$vehicule->model}}</h1>
+    </div>
+    <div class="offer_media">
+      {{-- <img src="{{asset('pubclic/images/vehicules'.$vehicule->imagePath)}}" alt=""> --}}
+      <img src="{{asset('images/view-offers_hero.jpg')}}" alt="">
+    </div>
+    <div class="offer_details">
+      <ul>
+        <li>color: {{$vehicule->color}}</li>
+        <li>type: {{$vehicule->type}}</li>
+        <li>fuel: {{$vehicule->fuel}}</li>
+        <li>year: {{$vehicule->year}}</li>
+        <li>Gear type: {{$vehicule->gearType}}</li>
+        <li>doors number: {{$vehicule->doorsNb}}</li>
+        <li>Horse power: {{$vehicule->horsePower}}</li>
+        <li>air cooling: {{$vehicule->airCooling}}</li>
+        <li>Rating: {{$vehicule->rating}}</li>
+      </ul>
+    </div>
+    <form action="">
+      
+    </form>
   </div>
 </div>
 @endsection
