f78f624ae9477834c7cb553de4b66a27d4949393
hacene <cenha06@gmail.com>
2022-05-30 18:16:10 +0100

    what is eazyrent

--- resources/views/guestHome.blade.php
+++ resources/views/guestHome.blade.php
@@ -54,82 +54,72 @@
   </div>
   </form>
 </div>
-</div>
-</div>
-<div class="why-us d-flex align-items-center justify-content-center flex-column" style="margin-bottom: 60px">
-  <h1 style="margin-bottom: 40px">Why us ?</h1>
-  <div class="cards-container d-flex align-items-center flex-wrap justify-content-center mb-5">
-    <div class="card d-flex flex-column">
-      <i class="bi bi-square-fill"></i>
-      <h1>Lorem ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-        facilis magni blanditiis enim tempora quis.
-      </p>
-    </div>
-    <div class="card d-flex flex-column">
-      <i class="bi bi-square-fill"></i>
-      <h1>Lorem ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-        facilis magni blanditiis enim tempora quis.
-      </p>
-    </div>
-    <div class="card d-flex flex-column">
-      <i class="bi bi-square-fill"></i>
-      <h1>Lorem ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-        facilis magni blanditiis enim tempora quis.
-      </p>
+<div id="whoUs" class="who-us">
+  <div class="white-space"></div>
+  <div class="container">
+    <h2 class="section-header">what is <span>EAZYRENT</span></h2>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <div class="d-flex justify-content-center align-items-center">
+          <object data="{{asset('images/who-us.svg')}}" width="200" height="200"></object>
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <p><span>EazyRent</span> is a platform that makes renting your next vehicle a simple task in an easy, fast and secure way.
+        <br><br>
+        Our goal is to gather agencies from all around the nation in one place.
+        <br><br>
+        To give you <span>the best options with the least effort.</span>
+        </p>
+      </div>
     </div>
   </div>
-  <div id="carouselExampleIndicators" class="carousel slide w-100" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-        aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-        aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-        aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <div class="wrapper d-flex justify-content-center align-items-center flex-column">
-          <i class="bi bi-square-fill"></i>
-          <h1>Lorem ipsum</h1>
-          <p>Lorem ipsum dolor sit amet , adipisicing elit. Assumenda facilis magni blanditiis enim tempora quis.
-          </p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="wrapper d-flex justify-content-center align-items-center flex-column">
-          <i class="bi bi-square-fill"></i>
-          <h1>Lorem ipsum</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facilis magni blanditiis enim
-            tempora quis.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="wrapper d-flex justify-content-center align-items-center flex-column">
-          <i class="bi bi-square-fill"></i>
-          <h1>Lorem ipsum</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facilis magni blanditiis enim
-            tempora quis.</p>
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+  <div class="white-space"></div>
+</div>
+<div id="carouselExampleIndicators" class="carousel slide w-100" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+      aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+      aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+      aria-label="Slide 3"></button>
   </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="wrapper d-flex justify-content-center align-items-center flex-column">
+        <i class="bi bi-square-fill"></i>
+        <h1>Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet , adipisicing elit. Assumenda facilis magni blanditiis enim tempora quis.
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="wrapper d-flex justify-content-center align-items-center flex-column">
+        <i class="bi bi-square-fill"></i>
+        <h1>Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facilis magni blanditiis enim
+          tempora quis.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="wrapper d-flex justify-content-center align-items-center flex-column">
+        <i class="bi bi-square-fill"></i>
+        <h1>Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facilis magni blanditiis enim
+          tempora quis.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </div>
 <div class="our-partners">
   <h1 style="margin-bottom: 40px">Our partners</h1>
@@ -141,7 +131,8 @@
   </div>
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js">
+</script>
 <script>
   let driverAge = document.getElementById('driverAge')
   let driverAgeCheckBox = document.getElementById('driverAgeCheckBox')
