7d763f4ddb3254d2e67b48d3cf1ded591a7332ed
hacene <cenha06@gmail.com>
2022-06-09 11:30:02 +0100

    owner register

--- resources/views/owners/register.blade.php
+++ resources/views/owners/register.blade.php
@@ -1,270 +1,170 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Signup</title>
-    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <style>
-      .hide{
-        position: absolute;
-       visibility: hidden;
-       z-index: -10;
-      }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Owner Register</title>
+  <link rel="stylesheet" href="{{ asset('css/authentication.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
-    <div>
-        <div
-          class="container d-flex justify-content-center align-items-center "
-          style="min-height: 100vh"
-        >
-        <div class="signup-panel d-flex align-items-center justify-content-center flex-column" >
-          <form
-            class=" d-flex align-items-center justify-content-center flex-column"
-            action="{{ route('owner.create') }}"
-            method="POST"
-            style="width: 100%"
-            enctype="multipart/form-data"
-          >
-          <h1 class="title fw-bold">Sign up</h1>
+  <div class="authentication row">
+    <div class="authentication_welcome col-12 col-md-4">
+      <div class="content">
+        <object data="{{asset('images/icons/hi-authentication.svg')}}" width="300" height="300" defer
+          loading="lazy"></object>
+        <h2 class="section-heading">Welcome!</h2>
+        <p>Please enter your personal info to join us</p>
+        <p>You already have an account? </p>
+        <a href="{{ route('workerLogin') }}" class="custom-btn">Log in</a>
+      </div>
+    </div>
+    <div class="authentication_panel col-12 col-md-8">
+      <h2 class="section-header">Join eazyrent</h2>
+      <div class="content containter-fluid">
+        <form class="form-authentication" action="{{ route('owner.create') }}" method="POST"
+          enctype="multipart/form-data">
+          @csrf
           @if (Session::get('fail'))
           <div class="alert alert-danger w-100" role="alert">
-              {{ Session::get('fail') }}
-            </div>
+            {{ Session::get('fail') }}
+          </div>
           @endif
           @if (Session::get('success'))
           <div class="alert alert-success w-100 " role="alert">
             {{ Session::get('success') }}
           </div>
           @endif
-          @csrf
-            <div class="form-login d-flex flex-column w-100 signupStep">
-              <label for="">First name</label>
-              <input
-                type="text"
-                name="firstName"
-                maxlength="45"
-                class="inputs"
-                value="{{ old('firstName') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                    @error('firstName')
-                        {{ $message }}
-                    @enderror
-              </span>
-
-              <label for="">Last name</label>
-              <input
-                type="text"
-                name="lastName"
-                maxlength="45"
-                class="inputs"
-                value="{{ old('lastName') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('lastName')
-                    {{ $message }}
-                @enderror
-                </span>
-              <label for="">Birth Date</label>
-              <input
-              class="inputs"
-                type="date"
-                name="birthDate"
-                max="2003-01-01"
-                value="{{ old('birthDate') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('birthDate')
-                    {{ $message }}
-                @enderror
-                 </span>
-              <label for="">Address</label>
-              <input
-              class="inputs"
-                type="text"
-                name="address"
-                value="{{ old('address') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('address')
-                    {{ $message }}
-                @enderror
-                </span>
-                <p class="my-2">
-                  Already have an account?
-                  <a href="{{ route('workerLogin') }}" class="text-decoration-underline">Log in</a>
-              </p>
-              <p class="step-index">1/3</p>
-            </div>
-            <div class="form-login d-flex flex-column signupStep hide"  >
-              <label for="">Username</label>
-              <input
-                class="inputs"
-                type="text"
-                name="username"
-                value="{{ old('username') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('username')
-                    {{ $message }}
-                @enderror
-               </span>
-              <label for="">E-mail</label>
-              <input
-                class="inputs"
-                type="email"
-                value="{{ old('email') }}"
-                name="email"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('email')
-                    {{ $message }}
-                @enderror
-                </span>
-              <label for=""
-                >Phone number
-                <span style="font-size: 10px; color: gray">optional</span></label
-              >
-              <input
-                class="inputs"
-                type="text"
-                maxlength="10"
-                name="phone"
-                value="{{ old('phone') }}"
-                onkeypress="return isNumber(event)"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('phone')
-                    {{ $message }}
-                @enderror
-                </span>
-              <div class="d-flex flex-column">
-                <label for="">Password</label>
-                <input
-                  class="inputs"
-                  type="password"
-                  name="password"
-                />
-                <span class="text-danger" style="font-size:0.8rem">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-              </span>
-                <label for="">Password confirmation</label>
-                <input
-                  class="inputs"
-                  type="password"
-                  name="passwordConfirm"
-                />
-                <span class="text-danger" style="font-size:0.8rem">
-                    @error('passwordConfirm')
-                        {{ $message }}
-                    @enderror
-              </span>
-              </div>
-              <p class="step-index">2/3</p>
-            </div>
-            <div class="form-login d-flex flex-column signupStep hide">
-              <label for=""> Identity card number</label>
-              <input
-                class="inputs"
-                type="text"
-                maxlength="18"
-                name="idCard"
-                value="{{ old('idCard') }}"
-                onkeypress="return isNumber(event)"
-              />
-
-              <span class="text-danger" style="font-size:0.8rem">
-                @error('idCard')
-                  {{ $message }}
-                @enderror
-              </span>
-
-
-              <label for="">Upload an image of your identity card</label>
-              <input
-                class="inputs file-input"
-                type="file"
-                accept="image/*"
-                name="idCardImage"
-                style="display: none"
-                id="file-field"
-                value="{{ old('idCardImage') }}"
-              />
-              <span class="text-danger" style="font-size:0.8rem">
-              @error('idCardImage')
-                {{ $message }}
+          <div class="form-authentication signupStep">
+            <label for="">First name</label>
+            <input type="text" name="firstName" maxlength="45" class="inputs" value="{{ old('firstName') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('firstName')
+              {{ $message }}
               @enderror
             </span>
-              <div
-
-                class="image-selector"
-                alt=""
-                onclick="openFilePicker()"
-              ></div>
-              <img
-              onclick="openFilePicker()"
-                class="image-preview"
-                alt=""
-              />
-              <div class="d-flex justify-content-end ">
-                <input
-                  style="margin-top: 15px !important; width: 150px;"
-                  type="submit"
-                  value="Sign up!"
-                  class="custom-btn"
-                />
-              </div>
-              <p class="step-index">3/3</p>
+            <label for="">Last name</label>
+            <input type="text" name="lastName" maxlength="45" class="inputs" value="{{ old('lastName') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('lastName')
+              {{ $message }}
+              @enderror
+            </span>
+            <label for="">Birth Date</label>
+            <input class="inputs" type="date" name="birthDate" max="2003-01-01" value="{{ old('birthDate') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('birthDate')
+              {{ $message }}
+              @enderror
+            </span>
+            <label for="">Address</label>
+            <input class="inputs" type="text" name="address" value="{{ old('address') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('address')
+              {{ $message }}
+              @enderror
+            </span>
+            <p class="step-index">1/3</p>
+          </div>
+          <div class="form-authentication signupStep hide">
+            <label for="">Username</label>
+            <input class="inputs" type="text" name="username" value="{{ old('username') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('username')
+              {{ $message }}
+              @enderror
+            </span>
+            <label for="">E-mail</label>
+            <input class="inputs" type="email" value="{{ old('email') }}" name="email" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('email')
+              {{ $message }}
+              @enderror
+            </span>
+            <label for="">Phone number
+              <span style="font-size: 10px; color: gray">optional</span></label>
+            <input class="inputs" type="text" maxlength="10" name="phone" value="{{ old('phone') }}"
+              onkeypress="return isNumber(event)" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('phone')
+              {{ $message }}
+              @enderror
+            </span>
+            <div class="d-flex flex-column">
+              <label for="">Password</label>
+              <input class="inputs" type="password" name="password" />
+              <span class="text-danger" style="font-size:0.8rem">
+                @error('password')
+                {{ $message }}
+                @enderror
+              </span>
+              <label for="">Password confirmation</label>
+              <input class="inputs" type="password" name="passwordConfirm" />
+              <span class="text-danger" style="font-size:0.8rem">
+                @error('passwordConfirm')
+                {{ $message }}
+                @enderror
+              </span>
             </div>
-          </form>
-          <div class="d-flex justify-content-between w-100">
-            <button
-                    style="margin-top: 15px; width: 150px"
-                    class="custom-btn"
-                    id="prev"
-                  >
-                    Previous
-                  </button>
-                  <button
-                    style="margin-top: 15px; width: 150px; align-self: flex-end"
-                    class="custom-btn"
-                    id="next"
-                  >
-                    Next
-                </button>
-              </div>
-
-
-        </div>
-        </div>
+            <p class="step-index">2/3</p>
+          </div>
+          <div class="form-authentication signupStep hide">
+            <label for=""> Identity card number</label>
+            <input class="inputs" type="text" maxlength="18" name="idCard" value="{{ old('idCard') }}"
+              onkeypress="return isNumber(event)" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('idCard')
+              {{ $message }}
+              @enderror
+            </span>
+            <label for="">Upload an image of your identity card</label>
+            <input class="inputs file-input" type="file" accept="image/*" name="idCardImage" style="display: none"
+              id="file-field" value="{{ old('idCardImage') }}" />
+            <span class="text-danger" style="font-size:0.8rem">
+              @error('idCardImage')
+              {{ $message }}
+              @enderror
+            </span>
+            <div class="image-selector" alt="" onclick="openFilePicker()"></div>
+            <img onclick="openFilePicker()" class="image-preview" alt="" />
+            <p class="step-index">3/3</p>
+          </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <button class="custom-btn custom-btn-dark" id="prev" onclick="event.preventDefault()" type="text">
+              Previous
+            </button>
+            <button class="custom-btn custom-btn-dark" id="next" onclick="event.preventDefault()" type="text">
+              Next
+            </button>
+            <input type="submit" class="custom-btn custom-btn-dark d-none" id="submit-button" value="Sign up">
+          </div>
+        </form>
       </div>
-      <script>
-        const pages = document.querySelectorAll('.signupStep')
+    </div>
+  </div>
+  <script>
+    const pages = document.querySelectorAll('.signupStep')
         const stepInc = document.querySelector('#next')
         const stepDec = document.querySelector('#prev')
-
+        const submitBtn = document.querySelector('#submit-button')
         let currentStep=0;
         stepDec.disabled=true;
-        // stepDec.style.backgroundColor ='gray'
         stepInc.addEventListener('click',()=>{
           currentStep++;
           pages.forEach(element => {
             element.classList.add('hide');
           });
-            pages[currentStep].classList.remove("hide");
+          pages[currentStep].classList.remove("hide");
           if(currentStep==2){
-            // stepInc.style.backgroundColor ='gray'
             stepInc.disabled=true;
+            stepInc.classList.add('d-none')
+            submitBtn.classList.remove('d-none')
           }
           if(currentStep!=2){
-            // stepDec.style.backgroundColor ='rgb(187, 190, 105)'
             stepDec.disabled=false;
           }
         });
@@ -276,12 +176,12 @@
           });
             pages[currentStep].classList.remove("hide");
           if(currentStep==0){
-            // stepDec.style.backgroundColor ='gray'
             stepDec.disabled=true;
           }
           if(currentStep!=2){
-            // stepInc.style.backgroundColor ='rgb(187, 190, 105)'
             stepInc.disabled=false;
+            stepInc.classList.remove('d-none')
+            submitBtn.classList.add('d-none')
           }
         });
 
@@ -331,10 +231,10 @@
     function openFilePickerFace(){
       document.getElementById("file-field-face").click()
     }
-      </script>
-      {{-- <script src="https://unpkg.com/vue@3"></script>
-      <script>
-        Vue.createApp({
+  </script>
+  {{-- <script src="https://unpkg.com/vue@3"></script>
+  <script>
+    Vue.createApp({
             data() {
                 return {
                     signUpStep: 1,
@@ -358,6 +258,7 @@
             },
         },
         }).mount('#app')
-    </script> --}}
+  </script> --}}
 </body>
+
 </html>
