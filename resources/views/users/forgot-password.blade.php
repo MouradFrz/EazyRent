df1d9fe93545dea9805de08e0d2375def461be7f
hacene <cenha06@gmail.com>
2022-06-09 12:16:58 +0100

    reset password

--- resources/views/users/forgot-password.blade.php
+++ resources/views/users/forgot-password.blade.php
@@ -1,18 +1,31 @@
-
-
-
-
-@if (Session::get('status'))
-{{ Session::get('status') }} 
-<br>
-@else
-<form action="{{ route('password.request') }}" method="post">@csrf
-    <input type="text" name="email">
-    <button type="submit">submit</button>
-@error('email')
-    {{ $message }}
-@enderror
-    
-</form>
-
-@endif
+@extends('layouts.userLayout')
+@section('head')
+@endsection
+@section('content')
+<div class="white-space"></div>
+<div class="resset-password">
+  <div class="content">
+    <h1 class="text-center">EazyRent</h1>
+    <h3 class="text-center">resert your passowrd</h3>
+    @if (Session::get('status'))
+    <p class="text-center">
+      {{ Session::get('status') }}
+    </p>
+    @else
+    <p class="mb-4 mt-4">Enter the email address associated with your account and we'll send you a link to reset your password</p>
+    <form action="{{ route('password.request') }}" method="post" enctype="multipart/form-data" class="form resset-password-form">
+      @csrf
+      <label for="email" class="label">email</label>
+      <input type="text" name="email" class="inputs">
+      @error('email')
+      <p class="danger">{{ $message }}</p>
+      @enderror
+      <div class="d-flex justify-content-center">
+        <button type="submit" class="custom-btn custom-btn-dark mt-3">submit</button>
+      </div>
+    </form>
+    @endif
+  </div>
+</div>
+<div class="white-space"></div>
+@endsection
--- resources/views/users/reset-password.blade.php
+++ resources/views/users/reset-password.blade.php
@@ -1,17 +1,24 @@
+@extends('layouts.userLayout')
+@section('head')
+@endsection
+@section('content')
+<div class="test">
+</div>
 <form action="{{ route('password.update') }}" method="post">
-    @csrf
-<input type="text" placeholder="Email" name="email">
-@error('email')
-    {{ $message }}
-@enderror
-<input type="text" placeholder="Password" name="password">
-@error('password')
-    {{ $message }}
-@enderror
-<input type="text" placeholder="Confirm Password" name="password_confirmation">
-@error('password_confirmation')
-    {{ $message }}
-@enderror
-<input type="text" name="token" style="display: none" value="{{ $token }}" id="">
-<button type="submit">Submit</button>
+  @csrf
+  <input type="text" placeholder="Email" name="email">
+  @error('email')
+  {{ $message }}
+  @enderror
+  <input type="text" placeholder="Password" name="password">
+  @error('password')
+  {{ $message }}
+  @enderror
+  <input type="text" placeholder="Confirm Password" name="password_confirmation">
+  @error('password_confirmation')
+  {{ $message }}
+  @enderror
+  <input type="text" name="token" style="display: none" value="{{ $token }}" id="">
+  <button type="submit">Submit</button>
 </form>
+@endsection
