116cfb1d8c22db39d73fe4581a89e63b913e8e46
hacenbarb <cenha06@gmail.com>
2023-02-25 13:46:36 +0100

    Migrating from Vite to Laravel Mix

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+  .js('resources/js/carslist.js', 'public/js')
+  .sass('resources/sass/bootstrap.scss', 'public/css')
+
+  .sass('resources/sass/app.scss', 'public/css')
+  .sass('resources/sass/worker.scss', 'public/css/worker')
+  .sass('resources/sass/authentication.scss', 'public/css')
+  .sass('resources/sass/register.scss', 'public/css')
+
+  .sass('resources/sass/user/index.scss', 'public/css/user')
+  .sass('resources/sass/admin/index.scss', 'public/css/admin')
+  .sass('resources/sass/owner/index.scss', 'public/css/owner')
+  .sass('resources/sass/secretary/index.scss', 'public/css/secretary')
+  .sass('resources/sass/garagist/index.scss', 'public/css/garagist')
+  .sass('resources/sass/vehicules/index.scss', 'public/css/vehicules')
+.sourceMaps();
+
+// auto reload
+mix.browserSync('localhost:8000');
